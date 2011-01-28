ffe72735ec0b63ff7826d015e51ac6e16e1b4b6a
Anup Dasari <adasari@codeaurora.org>
2011-01-28 18:07:18 +0530

    system/vold: Fix the issue of auto mount fail after formatting sdcard
    Auto mount fail due to curruption of partition table after formatting sdcard.
    MBR initialization is not done because of previous change
    to support format of emmc sdcard partition on 8x55.
    
    fix the issue by checking card type while formatting.
    
    CRs-fixed: 268627, 270858
    (cherry picked from commit 3036fdfbacf58395089de58e499befac42e9e028)
    
    Change-Id: I9398d545c65ad36ba8d76b89c9e71788350b7f1a

--- Volume.h
+++ Volume.h
@@ -25,7 +25,6 @@ class VolumeManager;
 class Volume {
 private:
     int mState;
-    int mPartitionNumber;
 
 public:
     static const int State_Init       = -1;
@@ -53,6 +52,7 @@ protected:
     VolumeManager *mVm;
     bool mDebug;
     int mPartIdx;
+    int mPartitionNumber;
 
     /*
      * The major/minor tuple of the currently mounted filesystem.
@@ -71,7 +71,7 @@ public:
     const char *getMountpoint() { return mMountpoint; }
     int getState() { return mState; }
 
-    int getOverrideSDPartition() { return mPartitionNumber; }
+    int getOverrideSDPartition() { return getEmmcCardVal() ? mPartitionNumber : 0; }
     void setOverrideSDPartition(int partitionNumber);
     virtual int handleBlockEvent(NetlinkEvent *evt);
     virtual dev_t getDiskDevice();
@@ -85,6 +85,7 @@ public:
 protected:
     void setState(int state);
 
+    virtual bool getEmmcCardVal() = 0;
     virtual int getDeviceNodes(dev_t *devs, int max) = 0;
 
     int createDeviceNode(const char *path, int major, int minor);
