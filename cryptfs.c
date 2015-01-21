0bbea4343fe4e2722ae1e07eb225bbe9aaccead2
David Ng <dave@codeaurora.org>
2015-01-21 13:55:21 -0800

    vold: Retry opening block device on failure when starting encryption
    The device mapper storage device node can take some time to be
    created; so retry.
    
    CRs-Fixed: 787512
    Change-Id: Ieeb3b697f9cef72d4ea9d106750696901f0a224d

--- cryptfs.c
+++ cryptfs.c
@@ -2485,37 +2485,6 @@ errout:
     return rc;
 }
 
-#define RETRY_OPEN_CRYPTO_BLK 10
-
-enum InplaceType {
-    INPLACE_EXT4,
-    INPLACE_F2FS,
-    INPLACE_FULL
-};
-
-static int cryptfs_open_crypto_blkdev(char *crypto_blkdev, enum InplaceType type) {
-    int fd, retry_count = 1;
-    while (1) {
-        if (type == INPLACE_EXT4 || type == INPLACE_FULL) {
-            fd = open(crypto_blkdev, O_WRONLY);
-        } else {//type == INPLACE_F2FS
-            fd = open64(crypto_blkdev, O_WRONLY);
-        }
-        if (fd < 0) {
-            if (retry_count == RETRY_OPEN_CRYPTO_BLK) {
-                return -1;
-            }
-            SLOGE("Error opening crypto_blkdev %s. Retried %d times, err=%d(%s)\n",
-                  crypto_blkdev, retry_count, errno, strerror(errno));
-            retry_count++;
-            usleep(2000);
-            continue;
-        }
-        break;
-    }
-    return fd;
-}
-
 static int cryptfs_enable_inplace_ext4(char *crypto_blkdev,
                                        char *real_blkdev,
                                        off64_t size,
@@ -2543,12 +2512,19 @@ static int cryptfs_enable_inplace_ext4(char *crypto_blkdev,
         goto errout;
     }
 
-    SLOGI("Opening crypto_blkdev %s for ext4 inplace encrypt.", crypto_blkdev);
-    if ( (data.cryptofd = cryptfs_open_crypto_blkdev(crypto_blkdev, INPLACE_EXT4)) < 0) {
-        SLOGE("Error opening crypto_blkdev %s for ext4 inplace encrypt. err=%d(%s)\n",
-              crypto_blkdev, errno, strerror(errno));
-        rc = ENABLE_INPLACE_ERR_DEV;
-        goto errout;
+    // Wait until the block device appears.  Re-use the mount retry values since it is reasonable.
+    int retries = RETRY_MOUNT_ATTEMPTS;
+    while ((data.cryptofd = open(crypto_blkdev, O_WRONLY)) < 0) {
+        if (--retries) {
+            SLOGE("Error opening crypto_blkdev %s for ext4 inplace encrypt. err=%d(%s), retrying\n",
+                  crypto_blkdev, errno, strerror(errno));
+            sleep(RETRY_MOUNT_DELAY_SECONDS);
+        } else {
+            SLOGE("Error opening crypto_blkdev %s for ext4 inplace encrypt. err=%d(%s)\n",
+                  crypto_blkdev, errno, strerror(errno));
+            rc = ENABLE_INPLACE_ERR_DEV;
+            goto errout;
+        }
     }
 
     if (setjmp(setjmp_env)) {
@@ -2675,8 +2651,7 @@ static int cryptfs_enable_inplace_f2fs(char *crypto_blkdev,
         goto errout;
     }
 
-    SLOGI("Opening crypto_blkdev %s for f2fs inplace encrypt.", crypto_blkdev);
-    if ( (data.cryptofd = cryptfs_open_crypto_blkdev(crypto_blkdev, INPLACE_F2FS)) < 0) {
+    if ( (data.cryptofd = open64(crypto_blkdev, O_WRONLY)) < 0) {
         SLOGE("Error opening crypto_blkdev %s for f2fs inplace encrypt. err=%d(%s)\n",
               crypto_blkdev, errno, strerror(errno));
         rc = ENABLE_INPLACE_ERR_DEV;
@@ -2749,8 +2724,7 @@ static int cryptfs_enable_inplace_full(char *crypto_blkdev, char *real_blkdev,
     }
 
 
-    SLOGI("Opening crypto_blkdev %s for inplace encrypt.", crypto_blkdev);
-    if ( (cryptofd = cryptfs_open_crypto_blkdev(crypto_blkdev, INPLACE_FULL)) < 0) {
+    if ( (cryptofd = open(crypto_blkdev, O_WRONLY)) < 0) {
         SLOGE("Error opening crypto_blkdev %s for inplace encrypt. err=%d(%s)\n",
               crypto_blkdev, errno, strerror(errno));
         close(realfd);
